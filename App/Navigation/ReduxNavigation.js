948167a9c5b9d7f52c2f0c251e327152fc0d2d3d
David Wu <david10608@gmail.com>
2018-02-14 16:52:44 +0100

    Support react-navigation beta.30

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -3,12 +3,26 @@ import * as ReactNavigation from 'react-navigation'
 import { connect } from 'react-redux'
 import AppNavigation from './AppNavigation'
 
-// here is our redux-aware smart component
+// for react-navigation 1.0.0-beta.30
+import {
+  createReduxBoundAddListener,
+  createReactNavigationReduxMiddleware,
+} from 'react-navigation-redux-helpers';
+
+const middleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.nav,
+);
+const addListener = createReduxBoundAddListener("root");
+// end for react-navigation 1.0.0-beta.30
+
+
 function ReduxNavigation (props) {
   const { dispatch, nav } = props
   const navigation = ReactNavigation.addNavigationHelpers({
     dispatch,
-    state: nav
+    state: nav,
+    addListener // <-- add this as well
   })
 
   return <AppNavigation navigation={navigation} />
