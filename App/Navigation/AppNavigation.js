7e471c2b903100f079696f6efcb5adc5192bbc46
David Wu <david10608@gmail.com>
2018-02-14 16:52:34 +0100

    Navigation for Footer Tabs

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,26 @@
-import { StackNavigator } from 'react-navigation'
-import LaunchScreen from '../Containers/LaunchScreen'
+import React from 'react';
+import { TabNavigator } from 'react-navigation';
+import AboutScreen from '../Containers/AboutScreen';
+import SettingsScreen from '../Containers/SettingsScreen';
+import HomeScreen from '../Containers/HomeScreen';
 
-import styles from './Styles/NavigationStyles'
+import styles from './Styles/NavigationStyles';
+import MainTabsFooter from '../Components/MainTabsFooter';
 
 // Manifest of possible screens
-const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+const PrimaryNav = TabNavigator({
+  HomeScreen: { screen: HomeScreen },
+  SettingsScreen: { screen: SettingsScreen },
+  AboutScreen: { screen: AboutScreen },
 }, {
+  tabBarPosition: 'bottom',
+  tabBarComponent: props => <MainTabsFooter {...props} />,
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'HomeScreen',
   navigationOptions: {
-    headerStyle: styles.header
-  }
-})
+    headerStyle: styles.header,
+  },
+});
 
-export default PrimaryNav
+export default PrimaryNav;
