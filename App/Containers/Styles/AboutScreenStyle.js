f82da6da5f78c9ab39873f27aad0243cf90feaa3
David Wu <david10608@gmail.com>
2018-02-14 19:43:14 +0100

    header styling

--- App/Containers/Styles/AboutScreenStyle.js
+++ App/Containers/Styles/AboutScreenStyle.js
@@ -1,6 +1,11 @@
-import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { StyleSheet } from 'react-native';
+import { ApplicationStyles } from '../../Themes/';
+
+import colors from '../../Themes/Colors';
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  ...ApplicationStyles.screen,
+  header: {
+    backgroundColor: colors.secondary,
+  },
+});
