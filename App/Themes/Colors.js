bec4a3ce984d5578ba2351c4a7933b263b48e8eb
David Wu <david10608@gmail.com>
2018-02-14 19:43:24 +0100

    add primary and secondary colors

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -21,7 +21,9 @@ const colors = {
   eggplant: '#251a34',
   border: '#483F53',
   banner: '#5F3E63',
-  text: '#E0D7E5'
-}
+  text: '#E0D7E5',
+  primary: '#6495ed',
+  secondary: '#435d8c',
+};
 
-export default colors
+export default colors;
