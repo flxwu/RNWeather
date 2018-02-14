a45e79506af10b0ac78c996281dba4124c7641f6
David Wu <david10608@gmail.com>
2018-02-14 19:43:03 +0100

    Header

--- App/Containers/SettingsScreen.js
+++ App/Containers/SettingsScreen.js
@@ -1,32 +1,23 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { Container, Header, Body, Title } from 'native-base';
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
 // Styles
-import styles from './Styles/SettingsScreenStyle'
+import styles from './Styles/SettingsScreenStyle';
 
 class SettingsScreen extends Component {
-  render () {
+  render() {
     return (
-      <ScrollView style={styles.container}>
-        <KeyboardAvoidingView behavior='position'>
-          <Text>SettingsScreen</Text>
-        </KeyboardAvoidingView>
-      </ScrollView>
-    )
+      <Container>
+        <Header style={styles.header}>
+          <Body style={{ alignItems: 'center' }}>
+            <Title style={{ fontWeight: 'bold' }}>Settings</Title>
+          </Body>
+        </Header>
+      </Container>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SettingsScreen)
+export default SettingsScreen;
