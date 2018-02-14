5cdf433f7cc287c8598932e6049098e4e9db0f55
David Wu <david10608@gmail.com>
2018-02-14 19:42:31 +0100

    footer tab colors

--- App/Components/Styles/MainTabsFooterStyle.js
+++ App/Components/Styles/MainTabsFooterStyle.js
@@ -1,10 +1,15 @@
 import { StyleSheet } from 'react-native';
 
+import colors from '../../Themes/Colors';
+
 export default StyleSheet.create({
   container: {
     flex: 1,
   },
   footerTab: {
-    backgroundColor: 'white',
+    backgroundColor: colors.secondary,
+  },
+  buttonText: {
+    color: colors.primary,
   },
 });
