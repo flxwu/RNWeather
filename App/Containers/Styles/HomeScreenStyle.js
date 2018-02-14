16cf08f0b99b09d1c8f38d858ca4e825cb975d91
David Wu <david10608@gmail.com>
2018-02-14 19:43:42 +0100

    weatherView styling

--- App/Containers/Styles/HomeScreenStyle.js
+++ App/Containers/Styles/HomeScreenStyle.js
@@ -1,6 +1,28 @@
-import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { StyleSheet } from 'react-native';
+import { ApplicationStyles } from '../../Themes/';
+
+import Metrics from '../../Themes/Metrics';
+
+import Colors from '../../Themes/Colors';
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  ...ApplicationStyles.screen,
+  body: {
+    backgroundColor: Colors.primary,
+  },
+  header: {
+    backgroundColor: Colors.secondary,
+  },
+  weatherView: {
+    alignItems: 'center',
+    marginTop: 2 * Metrics.section,
+  },
+  weatherDisplayText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
+  },
+});
