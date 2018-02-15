7f41254753570ce7e479fb968aacfdbd7fa0facb
David Wu <david10608@gmail.com>
2018-02-16 00:39:17 +0100

    settings screen styling

--- App/Containers/Styles/SettingsScreenStyle.js
+++ App/Containers/Styles/SettingsScreenStyle.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { ApplicationStyles } from '../../Themes/';
+import { ApplicationStyles, Metrics } from '../../Themes/';
 
 import colors from '../../Themes/Colors';
 
@@ -7,5 +7,25 @@ export default StyleSheet.create({
   ...ApplicationStyles.screen,
   header: {
     backgroundColor: colors.secondary,
+  },
+  form: {
+    backgroundColor: colors.secondary,
+    paddingBottom: Metrics.smallMargin,
+  },
+  formText: {
+    color: 'white',
+  },
+  saveButton: {
+    marginTop: Metrics.smallMargin,
+    backgroundColor: colors.primary,
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  inputContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: Metrics.doubleBaseMargin,
   },
 });
