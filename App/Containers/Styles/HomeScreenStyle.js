ea3bea63eabefb84283988411a7a02353fc9c84d
David Wu <david10608@gmail.com>
2018-02-16 00:39:09 +0100

    weather view styling

--- App/Containers/Styles/HomeScreenStyle.js
+++ App/Containers/Styles/HomeScreenStyle.js
@@ -14,15 +14,50 @@ export default StyleSheet.create({
     backgroundColor: Colors.secondary,
   },
   weatherView: {
-    alignItems: 'center',
-    marginTop: 2 * Metrics.section,
+    padding: Metrics.doubleBaseMargin,
+    paddingLeft: 2 * Metrics.doubleBaseMargin,
   },
   weatherDisplayText: {
     color: 'white',
     fontWeight: 'bold',
+    padding: Metrics.marginVertical,
+  },
+  weatherCityText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  weatherStateText: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  locationView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 15,
+  },
+  stateView: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   weatherIcon: {
     width: 100,
     height: 100,
+  },
+  tempDisplayText: {
+    color: 'white',
+    fontSize: 34,
+    padding: Metrics.doubleBaseMargin,
+    textAlign: 'center',
+  },
+  list: {
+    backgroundColor: Colors.secondary,
+    height: 40,
+  },
+  listCats: {
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
